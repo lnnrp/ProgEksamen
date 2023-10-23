93b1f5ffa86364df7fb22d72d2cb38c83c10f776
linnea <112690695+lnnrp@users.noreply.github.com>
2023-10-23 12:27:27 +0200

    linkedlist added

---
+++ ExamLibrary/AlgoritmerOgDatastrukturer/LinkedListNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AlgoritmerOgDatastrukturer
+{
+    public class Node<T>
+    {
+        public T Value { get; set; }
+        public Node<T> Next { get; set; }
+        public Node<T> Previous { get; set; }
+
+        public Node(T value)
+        {
+            Value = value;
+        }
+    }
+}
--- ExamLibrary/AlgoritmerOgDatastrukturer/MyLinkedList.cs
+++ ExamLibrary/AlgoritmerOgDatastrukturer/MyLinkedList.cs
@@ -1,8 +1,65 @@
 ﻿using System;
+using System.Collections;
 
 namespace AlgoritmerOgDatastrukturer
 {
-    public class MyLinkedList
+    public class MyLinkedList<T> : IEnumerable<T>
     {
+        public Node<T> First { get; set; }
+        public Node<T> Last { get; set; }
+
+        public Node<T> AddFirst(T value)
+        {
+            Node<T> n = new Node<T>(value);
+
+            if (First != null)
+            {
+                First.Previous = n;
+                n.Next = First;
+                First = n;
+            }
+            else
+            {
+                First = n;
+                Last = n;
+            }
+
+            return n;
+        }
+
+        public Node<T> AddLast(T value)
+        {
+            Node<T> n = new Node<T>(value);
+
+            if (First != null)
+            {
+                n.Previous = Last;
+                Last.Next = n;
+                Last = n;
+            }
+            else
+            {
+                First = n;
+                Last = n;
+            }
+
+            return n;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node<T> current = First;
+
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
