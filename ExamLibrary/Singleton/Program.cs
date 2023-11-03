bba1ba682db7d77ddd3da9a8edc10d52241eb4e7
linnea <112690695+lnnrp@users.noreply.github.com>
2023-11-03 15:35:13 +0100

    singleton added

---
+++ ExamLibrary/Singleton/Program.cs
@@ -0,0 +1,9 @@
+﻿using Singleton;
+
+Console.WriteLine("Current state: " + GameManager.Instance.State);
+
+GameManager.Instance.State = "Menu State";
+
+Console.WriteLine("\nCurrent state: " + GameManager.Instance.State);
+
+Console.ReadKey();
