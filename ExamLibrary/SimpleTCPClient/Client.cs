e5175b979d7e893288ebf7eddf57c60054672345
lnnrp <linnea.jklaarup@gmail.com>
2023-12-14 10:21:02 +0100

    simple tcp server

---
+++ ExamLibrary/SimpleTCPClient/Client.cs
@@ -0,0 +1,91 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace SimpleTCPClient
+{
+    internal class Client
+    {
+        /// <summary>
+        /// Bool to close the connection
+        /// </summary>
+        private static bool run = true;
+
+        static void Main(string[] args)
+        {
+            string server = "localhost";
+            int port = 6666;
+
+            Console.WriteLine("Waiting to connect to server ...");
+
+            using (TcpClient client = new TcpClient(server, port)) // Using statement gør bla at man ikke skal kalde dispose på client
+            {
+                using (NetworkStream stream = client.GetStream())
+                {
+                    // Two threads for both writing and reading
+                    Thread reader = new Thread(() => ReadMessage(stream));
+                    Thread writer = new Thread(() => WriteMessage(stream));
+
+                    reader.Start();
+                    writer.Start();
+
+                    reader.Join();
+                    writer.Join();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sends message to client
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="customer"></param>
+        /// <param name="message"></param>
+        private static void WriteMessage(NetworkStream stream)
+        {
+            while (run)
+            {
+                string message = Console.ReadLine(); // Blocking call to avoid infinite loop
+                // Encodes string to bytes to send
+                byte[] data = Encoding.UTF8.GetBytes(message);
+
+                // Writes data to the stream to send to client, uses byte arrray, 0 offset and length of array
+                stream.Write(data, 0, data.Length);
+
+                if (message.ToUpper() == "QUIT")
+                {
+                    run = false;
+                }
+            }
+
+        }
+
+        private static void ReadMessage(NetworkStream stream)
+        {
+            while (run)
+            {
+                if (stream.DataAvailable)
+                {
+                    byte[] data = new byte[256];
+
+                    int bytesRead = stream.Read(data, 0, data.Length);
+
+                    if (bytesRead > 0)
+                    {
+                        string responseData = Encoding.ASCII.GetString(data, 0, bytesRead);
+
+                        Console.WriteLine(responseData);
+                    }
+                    else
+                    {
+                        Thread.Sleep(100); // Performance
+                    }
+
+                    if (!stream.CanRead) 
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ExamLibrary/SimpleTCPServer/Server.cs
@@ -0,0 +1,150 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SimpleTCPServer
+{
+    internal class Server
+    {
+        private static TcpListener server;
+
+        private static int clientIndex;
+
+        static void Main(string[] args)
+        {
+            server = new TcpListener(IPAddress.Any, 6666);
+            server.Start();
+
+            Console.WriteLine("Server started ...");
+
+            while (true)
+            {
+                Console.WriteLine("Waiting on connection ...");
+
+                var client = server.AcceptTcpClient(); // Blocking call der venter på client connecter
+
+                ThreadPool.QueueUserWorkItem(HandleClient, client); // ThreadPool for all clients
+            }
+        }
+
+        private static void HandleClient(object obj)
+        {
+            TcpClient client = (TcpClient)obj;
+
+            try
+            {
+                NetworkStream stream = client.GetStream();
+
+                WriteMessage(stream, "Welcome to the server");
+
+                Client clients = new Client(clientIndex.ToString());
+                clientIndex++;
+
+                while (true)
+                {
+                    if (stream.DataAvailable)
+                    {
+                        string message = ReadMessage(stream, clients);
+
+                        Respond(stream, clients, message);
+
+                        if (message.ToUpper() == "QUIT")
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Thread.Sleep(100); // Sparer på ressoucer
+                        }
+
+                        if (!stream.CanRead)
+                        {
+                            break; // If there is nothing to read from stream (or not possible)
+                        }
+                    }
+                }
+
+            }
+            finally
+            {
+                client.Close(); // Slukker client
+            }
+        }
+
+        /// <summary>
+        /// Sends and echos messages back to client
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="custor"></param>
+        /// <returns></returns>
+        private string ReadMessage(NetworkStream stream, Client clients)
+        {
+            byte[] data = new byte[256];
+
+            int bytesRead = stream.Read(data, 0, data.Length);
+
+            if (bytesRead > 0) // if stream has any content
+            {
+                string clientData = Encoding.ASCII.GetString(data, 0, bytesRead);
+
+                Console.WriteLine($"Recieved: {clientData} from {clients.Name}");
+
+                return clientData;
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Chooses action based on client/customer input
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="clients"></param>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private string Respond(NetworkStream stream, Client clients, string input)
+        {
+            if (input.ToUpper() == "LIST") // Example of list input
+            {
+                string itemList = string.Empty;
+
+                itemList += "Write buy;item to buy something";
+
+                //foreach (var item in items)
+                //{
+                //    itemList += "\n" + item.Name; // Adds name of each item to one string
+                //}
+
+                WriteMessage(stream, itemList);
+            }
+
+            if (input.Contains(" "))
+            {
+                // Empty input
+            }
+
+            if (input.Contains("quit"))
+            {
+                return "Quit";
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Sends message to client
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="customer"></param>
+        /// <param name="message"></param>
+        private void WriteMessage(NetworkStream stream, string message)
+        {
+            // Encodes string to bytes to send
+            byte[] responseData = Encoding.UTF8.GetBytes(message);
+
+            // Writes data to the stream to send to client, uses byte arrray, 0 offset and length of array
+            stream.Write(responseData, 0, responseData.Length);
+            Console.WriteLine($"Sending: {message}");
+        }
+    }
+}
